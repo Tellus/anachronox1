f5e7d575cc86d23d980a135f825b0cefa292982e
Johannes Lindhart Borresen <johannes@the.homestead.dk>
2011-06-15 13:57:32 +0200

    (second commit attempt) Added Irrlicht 1.7.2 in the lib folder. Also added Irrlicht.NET wrapper.
    Added the ModelSupport project, meant to offer intermediate support for legacy MD2 models. The primary intent is to read Anachronox MD2 files into a somewhat generic data format, possibly Irrlicht's own; I'm thinking a partial class so we can expand the class with overloading export methods to whichever formats may be necessary.
    
    Current targets of work:
    - APE decompilation (either to pure-APE, or NuAPE... maybe Python/LUA?); IronPython might be perfect given the current .NET context.
    - MD2 decompilation as described above.

---
+++ ModelSupport/AnoxModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModelSupport
+{
+    /// <summary>
+    /// Class supporting the legacy Anachronox model format, based in md2.
+    /// </summary>
+    public class AnoxModel
+    {
+        /* Important note:
+         * char = 1byte;
+         * short = 2bytes;
+         * int = 4bytes;
+         * float = 4bytes;
+         */
+    }
+}
---
+++ ModelSupport/AnoxModelHeader.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModelSupport
+{
+    class AnoxModelHeader
+    {
+        /// <summary>
+        /// Denotes the size (in bytes) of the header. Very useful for loading binaries.
+        /// </summary>
+        public const Int32 HEADERSIZE = 16 * sizeof(Int32);
+
+        /// <summary>
+        /// Magic number... IDP2??
+        /// </summary>
+        public Int32 Ident;
+
+        /// <summary>
+        /// Version: must be 8 (according to MD2 specs)... possibly 9 in Anox?
+        /// </summary>
+        public Int32 Version;
+
+        /// <summary>
+        /// Texture Width;
+        /// </summary>
+        public Int32 SkinWidth;
+
+        /// <summary>
+        /// Texture Height;
+        /// </summary>
+        public Int32 SkinHeight;
+
+        /// <summary>
+        /// Size in bytes of a frame (wtf?!)
+        /// </summary>
+        public Int32 FrameSize;
+
+        /// <summary>
+        /// Number of skins.
+        /// </summary>
+        public Int32 SkinCount;
+
+        /// <summary>
+        /// Number of vertices per frame.
+        /// </summary>
+        public Int32 VerticeCount;
+
+        /// <summary>
+        /// Number of texture coordinates.
+        /// </summary>
+        public Int32 TextureCoordinateCount;
+
+        /// <summary>
+        /// Number of triangles.
+        /// </summary>
+        public Int32 TriangleCount;
+
+        /// <summary>
+        /// Number of GL commands (no clue).
+        /// </summary>
+        public Int32 GlCmdCount;
+
+        /// <summary>
+        /// Number of frames.
+        /// </summary>
+        public Int32 FrameCount;
+
+        /// <summary>
+        /// Offset skin data.
+        /// </summary>
+        public Int32 SkinsOffset;
+
+        /// <summary>
+        /// Offset texture coordinate data.
+        /// </summary>
+        public Int32 TextureCoordinateOffset;
+
+        /// <summary>
+        /// Offset frame data.
+        /// </summary>
+        public Int32 FramesOffset;
+        
+        /// <summary>
+        /// GL Command data offsets (again, wtf?!).
+        /// </summary>
+        public Int32 GlCmdData;
+
+        /// <summary>
+        /// Offset end of file.
+        /// </summary>
+        public Int32 EndOfFileOffset;
+    }
+}
